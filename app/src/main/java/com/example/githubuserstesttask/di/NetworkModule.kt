b741c942467695906ea43e655545ae52c217b20f
andrej.savchuk.110 <andrej.petrenko.110@gmail.com>
2023-03-11 20:29:50 +0200

    Added NetworkModule.kt and CoroutineModule.kt

--- app/src/main/java/com/example/githubuserstesttask/di/NetworkModule.kt
+++ app/src/main/java/com/example/githubuserstesttask/di/NetworkModule.kt
@@ -1,10 +1,46 @@
 package com.example.githubuserstesttask.di
 
+import com.example.githubuserstesttask.data.remote.UsersGitHubApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {
+class NetworkModule {
+
+
+    @Provides
+    @Singleton
+    fun provideClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitApi(retrofit: Retrofit): UsersGitHubApi{
+        return retrofit.create(UsersGitHubApi::class.java)
+    }
+
+    companion object{
+        private const val BASE_URL = "https://api.github.com/"
+    }
 }
